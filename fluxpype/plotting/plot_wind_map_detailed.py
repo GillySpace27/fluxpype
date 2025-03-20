f7ac5d151385d009c0f10f7bc680ff5117a5c1f9
gillyspace27 <gilly@swri.org>
2025-03-20 16:06:20 -0600

    remove backend specification

--- fluxpype/plotting/plot_wind_map_detailed.py
+++ fluxpype/plotting/plot_wind_map_detailed.py
@@ -26,11 +26,11 @@ Functions:
 
 import numpy as np
 import matplotlib as mpl
-try:
-    mpl.use("qt5agg")
-except ImportError as e:
-    print(e)
-    mpl.use("agg")
+# try:
+#     mpl.use("qt5agg")
+# except ImportError as e:
+#     print(e)
+#     mpl.use("agg")
 
 import matplotlib.pyplot as plt
 import argparse
