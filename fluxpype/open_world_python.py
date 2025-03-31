f5210ba61d54a6c4192f6d685d7ebd1feb33ac81
gillyspace27 <gilly@swri.org>
2025-03-31 11:25:51 -0600

    remove vestigial file

--- fluxpype/open_world_python.py
+++
@@ -1,7 +0,0 @@
-from fluxpype.pipe_helper import read_flux_world
-
-f_world = read_flux_world("/Users/cgilbert/vscode/fluxons/fluxpype/fluxpype/data/batches/Newtest/data/cr2225/world/cr2225_f2000_hmi_relaxed_s800.flux")
-
-print(f_world, "\n")
-print(f_world.fluxons, "\n")
-print(f_world.concentrations, "\n")
